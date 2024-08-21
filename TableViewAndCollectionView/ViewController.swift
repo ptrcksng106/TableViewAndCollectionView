e7c37f7b84d50055c9f4844b683c70612fa00007
Patrick Sinaga <sinagapatrick610@gmail.com>
2024-08-21 20:29:29 +0700

    Initial Commit

---
+++ TableViewAndCollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewAndCollectionView
+//
+//  Created by Patrick Samuel Owen Saritua Sinaga on 21/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
